e7f7494e8b10c17f5a27a822e2e423f86fc4ac75
Tonkla004 <115675957+Tonkla004@users.noreply.github.com>
2023-01-17 19:59:01 +0700

    twrp

--- omni_light.mk
+++ omni_light.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from light device
 $(call inherit-product, device/xiaomi/light/device.mk)
 
 PRODUCT_DEVICE := light
-PRODUCT_NAME := omni_light
+PRODUCT_NAME := twrp_light
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22041219G
 PRODUCT_MANUFACTURER := xiaomi
